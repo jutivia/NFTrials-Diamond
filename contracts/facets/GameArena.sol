d9f3ccd8a7c236872a434e304f2aca82d1fff225
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-03-12 23:25:13 +0100

    changes

--- contracts/facets/GameArena.sol
+++ contracts/facets/GameArena.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 import {AppStorage} from "../libraries/LibAppStorage.sol";
 import {LibGame} from "../libraries/LibAppGameFunctions.sol";
 contract GameArena {
